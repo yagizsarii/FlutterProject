45679f48829f655cc5d792bc02796871b7c1a131
yagizsarii <yagiz.sari@archerobotics.com>
2024-09-24 02:02:46 +0300

    feat: appbar created

--- flutter_project/lib/pages/home.dart
+++ flutter_project/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar()
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Poppins'),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg',
+                height: 20, width: 20)),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+              margin: const EdgeInsets.all(10),
+              alignment: Alignment.center,
+              width: 35,
+              decoration: BoxDecoration(
+                color: const Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SvgPicture.asset('assets/icons/dots.svg',
+                  height: 5, width: 5)),
+        )
+      ],
     );
   }
 }
